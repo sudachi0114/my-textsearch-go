8b92a97cadc27f3296d2f2e27410acce62019101
sudachi0114 <jnevariste@gmail.com>
2024-03-02 14:34:28 +0900

    コマンドライン引数で指定されたファイルを読み込んで標準出力に出す機能をつけた

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	println("hello world!")
+	// fmt.Println(os.Args[1])
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("[Error] file cannot open:", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lineNum := 1
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+
+		lineNum++
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("[Error] error occurred while reading file:", err)
+	}
 }
